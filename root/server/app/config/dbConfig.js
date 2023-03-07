47dc76e23fd0cb705cd7cd562815117cf553692c
wilfre-oss <63850794+wilfre-oss@users.noreply.github.com>
2023-03-07 15:41:14 +0100

    Update dbConfig.js

--- root/server/app/config/dbConfig.js
+++ root/server/app/config/dbConfig.js
@@ -3,6 +3,7 @@ module.exports = {
     USER: process.env.DB_USER, // Change this if you have another user
     PASSWORD: process.env.DB_PASSWORD,
     DB: process.env.DB_NAME, // Change this if you have another name for the database
+    PORT: process.env.DB_PORT,
     dialect: "mysql",
     pool: { // Will be used for Sequelize connection pool configuration
         max: 5, // Maximum number of connection in pool

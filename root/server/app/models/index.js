ae97d9c172824741217ac71bd2892aeb00f6f06b
wilfre-oss <63850794+wilfre-oss@users.noreply.github.com>
2023-03-07 16:23:44 +0100

    Update index.js

--- root/server/app/models/index.js
+++ root/server/app/models/index.js
@@ -11,6 +11,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
+    port: dbConfig.PORT,
     //operatorsAliases: false,
   
     pool: {
